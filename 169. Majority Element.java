444d227b7a038e0805c9557d75a2a73552317973
swapnil-deotare <swapnildeotare@gmail.com>
2022-02-21 16:12:24 -0800

    Boyer-Moore Voting Algorithm

---
+++ 169. Majority Element.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/majority-element/
+//  Boyer-Moore Voting Algorithm
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
