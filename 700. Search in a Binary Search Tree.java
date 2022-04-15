1ca6669d1570136661ea470b93819e36a361f01b
swapnil-deotare <swapnildeotare@gmail.com>
2022-04-15 11:45:44 -0700

    switching back to good old trees

---
+++ 700. Search in a Binary Search Tree.java
@@ -0,0 +1,6 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null || root.val == val) return root;
+        return root.val>val? searchBST(root.left,val) : searchBST(root.right,val); 
+    }
+}
