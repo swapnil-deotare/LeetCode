c8dad797688809e9ac7c0c8d6058682c3dab5371
swapnil-deotare <swapnildeotare@gmail.com>
2022-07-03 14:30:58 -0700

    LC 75

---
+++ 394. Decode String.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public String decodeString(String s) {
+      if(s==null || s.length()==0) return "";
+      Stack<Integer> counts = new Stack<>();
+      Stack<StringBuilder> values = new Stack<>();
+      values.push(new StringBuilder());
+      int num = 0;
+      for(int i=0;i<s.length();i++){
+          char ch = s.charAt(i);
+          if(ch>='0' && ch<='9'){
+              num = num*10+ (ch-'0');
+          } else if(ch=='['){
+              values.push(new StringBuilder());
+              counts.push(num);
+              num = 0; //reset for next nums
+          } else if(ch==']'){
+              String toAppend = values.pop().toString();
+              int count = counts.pop();
+              StringBuilder nsb = new StringBuilder();
+              for(int j=0;j<count;j++){
+                  nsb.append(toAppend);
+              }
+              values.push(values.pop().append(nsb.toString()));
+          }else{
+              values.peek().append(ch);
+          }
+      }
+        
+        return values.pop().toString();
+    }
+}
